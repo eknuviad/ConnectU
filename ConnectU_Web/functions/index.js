2ef9aa5084033ca7fb3a56b065610355aa81f258
Edem Nuviadenu <edemnuviadenu@gmail.com>
2021-01-02 15:02:31 -0500

    Add signup route

--- ConnectU_Web/functions/index.js
+++ ConnectU_Web/functions/index.js
@@ -6,8 +6,71 @@ const { database } = require('firebase-admin');
 admin.initializeApp();
 const db = admin.firestore()
 
+const firebase = require('firebase')
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBhK-w4mrwTqS58ky4AP6_IvDe_QgeJxzQ",
+    authDomain: "connectu-90578.firebaseapp.com",
+    projectId: "connectu-90578",
+    storageBucket: "connectu-90578.appspot.com",
+    messagingSenderId: "676134639577",
+    appId: "1:676134639577:web:d1bfea934c84fa3795abc8",
+    measurementId: "G-N5P0RMHE0E"
+  };
+firebase.initializeApp(firebaseConfig)
+
+//initialise express app
 const express = require('express');
 const app = express()
+
+
+//Signup route
+let token, userId
+app.post('/signup', (req,res)=>{
+    const newUser ={
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        userName: req.body.userName,
+    };
+    //TODO validate data
+    db.collection('users').doc(`${newUser.userName}`).get()
+        .then(doc=>{
+            if(doc.exists){
+                return res.status(400).json({
+                    userName: 'username is already taken'
+                })
+            }else{
+                return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+            }
+        })
+        .then(data => {
+            userId = data.user.uid;
+            return data.user.getIdToken() //get an access token of user which is used to access a protected route
+        })
+        .then(tokenID => {
+            token = tokenID;
+            const userCredentials = {
+                email: newUser.email,
+                userName: newUser.userName,
+                createdDate: new Date().toISOString(),
+                userId
+            };
+            return db.collection('users').doc(`${newUser.userName}`).set(userCredentials);
+        })
+        .then(()=>{
+            return res.status(200).json({token});
+        })
+        .catch(err => {
+            console.error(err)
+            return res.status(500).json({
+                error: err.code
+            })
+        })
+});
+
+
 
 //Connection APIs
 app.get('/connections', (req, res) => {
@@ -83,17 +146,6 @@ exports.api = functions.https.onRequest(app);
 //         })
 //         .catch (error => console.error);
 // })
-
-
-
-
-
-
-
-
-
-
-
 
 
 // // Create and Deploy Your First Cloud Functions
