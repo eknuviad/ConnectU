a868bd47732d23812ba70d3132bf3530276c9ef5
Edem Nuviadenu <edemnuviadenu@gmail.com>
2021-01-05 18:06:46 -0500

    working deployment

--- ConnectU_Web/functions/index.js
+++ ConnectU_Web/functions/index.js
@@ -17,6 +17,109 @@ const app = express()
 const cors = require("cors");
 app.use(cors({origin: true })); //to ensure that server works on cross platform
 
+/****User APIs ****/
+
+//middleware authentication
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+        idToken = req.headers.authorization.split('Bearer ')[1]; //take second element which will be token
+    }else{
+        console.error('No token found')
+        return res.status(403).json({error: 'Unauthorized'});
+    }
+
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => { //holds data thats inside token ie userDATA
+            req.user = decodedToken;
+            console.log(decodedToken)
+            return db.collection('users')
+                .where('userId', '==', req.user.uid)
+                .limit(1)
+                .get();
+        })
+        .then(data => {
+            req.user.userName = data.docs[0].data().userName;
+            return next();
+        })
+        .catch(err =>{
+            console.error('Error while verifying token ', err);
+            return res.status(403).json(err);
+        })
+}
+
+//Signup route
+let token, userId
+app.post('/signup', (req,res)=>{
+    const newUser ={
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        userName: req.body.userName,
+    };
+    //TODO validate data
+    //need to ensure that the password and username not empty
+    //and that the two passwords match
+
+    db.collection('users').doc(`${newUser.userName}`).get()
+        .then(doc=>{
+            if(doc.exists){
+                return res.status(400).json({
+                    userName: 'username is already taken'
+                })
+            }else{
+                return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+            }
+        })
+        .then(data => {
+            userId = data.user.uid;
+            return data.user.getIdToken() //get an access token of user which is used to access a protected route
+        })
+        .then(tokenID => {
+            token = tokenID;
+            const userCredentials = {
+                email: newUser.email,
+                userName: newUser.userName,
+                createdDate: new Date().toISOString(),
+                userId
+            };
+            return db.collection('users').doc(`${newUser.userName}`).set(userCredentials);
+        })
+        .then(()=>{
+            return res.status(200).json({token});
+        })
+        .catch(err => {
+            console.error(err)
+            return res.status(500).json({
+                error: err.code
+            })
+        })
+});
+
+app.post('/login', (req, res) =>{
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    //TODO check that it isnt empty
+    
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then(data =>{
+            return data.user.getIdToken();
+        })
+        .then(token =>{
+            return res.status(200).json({token});
+        })
+        .catch(err =>{
+            console.error(err);
+            // if(err.code === 'auth/wrong-password'){
+            //     return res.status(403).json({general: 'Wrong credentials, please try again'});
+            // }
+            return res.status(500).json({error: err.code})
+        })
+})
+
 
 //Connection APIs
 app.get('/connections', (req, res) => {
@@ -37,6 +140,93 @@ app.get('/connections', (req, res) => {
         })
         .catch(error => console.error);
 })
+
+app.post('/connection', FBAuth, (req, res) => {
+    const newConnection =  {
+        sender: req.user.userName,
+        recipient: req.body.recipient,
+        createdDate: admin.firestore.Timestamp.fromDate(new Date()),
+        status: req.body.status,
+        isConnected: req.body.isConnected
+    };
+
+    //TODO validation that no connection has been sent to recipient already
+    //read ifrebase documentation for simple query
+    //write/add to database
+    db.collection('connections').add(newConnection)
+        .then(doc => {
+            return res.json ({message: `connection ${doc.id} created successfully`});
+        })
+        .catch(err =>{
+            res.status(500).json({
+                error: `Failed to setup new connection`
+            })
+            console.error(err);
+        })
+});
+
+//update a connection
+app.put('/connection', FBAuth, (req,res)=>{
+    db.collection("connections").doc(`${req.body.connectionId}`).update({
+        isConnected: req.body.isConnected,
+        status: req.body.status
+    })
+    .then(()=>{
+        return res.status(200).json({message: `successfully updated connection ${req.body.connectionId}`})
+    })
+    .catch(err =>{
+        console.log(err);
+        return res.status(500).json({
+            error: `Failed to update connection ${req.body.connectionId}`
+        })
+    })
+})
+
+//Questions APIs
+app.post('/question', FBAuth, (req,res)=>{
+    const newQuestion = {
+        question: req.body.question,
+        answer: req.body.answer,
+        sender: req.user.userName
+    }
+
+    db.collection('questions').add(newQuestion)
+        .then(doc => {
+            return res.json ({message: `question ${doc.id} created successfully`})
+        })
+        .catch(err =>{
+            console.error(err)
+            return res.status(500).json({
+                error: `Failed to add new question`
+            })
+        })
+})
+
+
+app.get('/question', FBAuth, (req,res)=>{
+    db.collection('questions')
+        .where('sender', '==', req.user.userName).get()
+        .then(snapShot =>{
+            let questionsData = [];
+            snapShot.forEach(doc =>{
+                console.log(doc.id, "=>", doc.data());
+                questionsData.push({
+                    questionId: doc.id,
+                    ...doc.data()
+                });
+            });
+            return res.json(questionsData)
+        })
+        .catch(err =>{
+            console.log(err);
+            return res.status(404).json({
+                error: `No questions found for ${req.user.userName}`
+            })
+        })
+})
+
+
+
 
 
 exports.api = functions.https.onRequest(app);
